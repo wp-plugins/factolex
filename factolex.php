11048392749d11dbe1814933cd69625360c8f82c
sebmos <sebmos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-26 00:18:46 +0000

    Update to version 2: add methods getArray(), getById(), getByTerm(), restructure parameters
    git-svn-id: https://plugins.svn.wordpress.org/factolex/trunk@97772 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- factolex.php
+++ factolex.php
@@ -2,123 +2,134 @@
 /*
 Plugin Name: Factolex
 Plugin URI: http://sebmos.at/factolex-wordpress-plugin/
-Description: Shows Factolex facts
+Description: Shows Factolex facts, requires minimum development skills
 Author: Sebastian Moser
-Version: 0.1
+Version: 0.2
 Author URI: http://sebmos.at/
 License: GPL v3 - http://www.gnu.org/licenses/gpl-3.0.html
 */ 
 
+define('FACTOLEX_NOFOLLOW',		0);
+
 class Factolex_Facts
 {
-	private $term;
+	private $query;
 	private $max_facts;
 	private $language;
 	private $terms		= array();
 	
-	function __construct($term, $max_facts = 3, $language = 'en')
-	{
-		$this->term			= $term;
-		$this->max_facts	= $max_facts;
-		$this->language		= $language;
-		
-		$this->getByTerm();
-	}
+	function __construct() { }
 	
-	private function getByTerm()
+	public function getByTerm($query, $language = 'en')
 	{
-		$this->search();return;
+		$this->query		= $query;
+		$this->terms		= array();
 		
 		$url = 'http://api.factolex.com/v1/get'
-			. '?term=' . urlencode(mb_strtolower($this->term))
-			. '&lang=' . $this->language
-			. '&format=xml';
+			. '?term=' . urlencode(mb_strtolower($this->query))
+			. '&lang=' . $language
+			. '&format=php';
 		
-		$xml = $this->xml2array($url);
+		$xml = $this->getResult($url);
 		
 		if (isset($xml['error']))
 		{
 			switch ($xml['error']['code'])
 			{
 				case 201:
-					// no term found, try to search for term
-					
-					$this->search();
+					// no term found
 					
 					break;
 					
 				case 202:
 					// multiple terms found, continue
 					
-					$this->addTerms($xml['error']['terms']['term']);
+					$this->addTerms($xml['error']['terms']);
 					
 					break;
 			}
 		}
 		else
 		{
-			$this->addTerm($xml['response']['term']);
+			$this->addTerm($xml['term']);
 		}
 	}
 	
-	private function getById($id)
+	public function getById($id, $reset = true)
 	{
+		if ($reset)
+			$this->terms		= array();
+		
 		$url = 'http://api.factolex.com/v1/get'
 			. '?id='		. $id
-			. '&amp;lang='	. $this->language
-			. '&format=xml';
+			. '&format=php';
 		
-		$xml = $this->xml2array($url);
+		$xml = $this->getResult($url);
 		
-		if (isset($xml['response']['term']['facts']['fact']))
-			$this->addTerm($xml['response']['term']);
+		if (!isset($xml['term']['facts']))
+			return;
+		
+		$this->addTerm($xml['term']);
 	}
 	
-	private function search()
+	private function getResult($url)
 	{
-		$url = 'http://api.factolex.com/v1/search'
-			. '?query=' . urlencode(mb_strtolower($this->term))
-			. '&lang=' . $this->language
-			. '&format=xml';
-		
-		$xml = $this->xml2array($url);
-		
-		if ($xml['response']['result']['term'])
+		$handler = fopen($url, 'rb');
+		$contents = '';
+		while (!feof($handler))
 		{
-			$this->addTerms($xml['response']['result']['term']);
+			$contents .= fread($handler, 8192);
+			continue;
+		}
+		
+		$xml = unserialize($contents);
+		
+		return $xml;
+	}
+	
+	public function search($query, $language = 'en')
+	{
+		$this->query		= $query;
+		$this->terms		= array();
+		
+		$url = 'http://api.factolex.com/v1/search'
+			. '?query=' . urlencode(mb_strtolower($this->query))
+			. '&lang=' . $language
+			. '&format=php';
+		
+		$xml = $this->getResult($url);
+		
+		if (count($xml['result']) > 0)
+		{
+			$this->addTerms($xml['result']);
 		}
 	}
 	
 	private function addTerms($terms)
 	{
-		// if only one term is returned, it's not in an array; fix that
-		$keys = array_keys($terms);
-		if (!is_numeric($keys[0]))
-			$terms = array($terms);
-		
 		foreach ($terms as $term)
 		{
-			$this->getById($term['id']);
+			$this->getById($term['id'], false);
 		}
 	}
 	
 	private function addTerm($term)
 	{
-		// if only one fact is returned, it's not in an array; fix that
-		$keys = array_keys($term['facts']['fact']);
-		if (!is_numeric($keys[0]))
-			$term['facts']['fact'] = array($term['facts']['fact']);
-		
 		$this->terms[] = $term;
 	}
 	
-	public function getHtml()
+	public function getArray()
+	{
+		return $this->terms;
+	}
+	
+	public function getHtml($max_facts)
 	{
 		// order terms by difference from search term
 		$terms_sorted = array();
 		foreach ($this->terms as $term)
 		{
-			$terms_sorted[levenshtein($this->term, $term['title'])] =
+			$terms_sorted[levenshtein($this->query, $term['title'])] =
 				$term;
 		}
 		ksort($terms_sorted);
@@ -128,14 +139,18 @@ class Factolex_Facts
 		
 		foreach ($terms_sorted as $term)
 		{
-			foreach ($term['facts']['fact'] as $fact)
+			foreach ($term['facts'] as $fact)
 			{
-				if (!($facts_count < $this->max_facts))
+				if (!($facts_count < $max_facts))
 					break 2;
 				
 				$output .= '<li>'
-						. '<a href="' . $term['link'] . '" rel="nofollow">'
-						. $term['title'] . '</a>: '
+						. '<a href="' . $term['link'] . '"';
+				
+				if (FACTOLEX_NOFOLLOW)
+					$output .= ' rel="nofollow"';
+				
+				$output .= '>' . $term['title'] . '</a>: '
 						. $fact['title']
 						. '</li>';
 				
@@ -148,144 +163,5 @@ class Factolex_Facts
 		
 		return
 			  '<ul>' . $output . '</ul>';
-	}
-	
-	private function xml2array($url, $get_attributes = 1, $priority = 'tag')
-	{
-	    $contents = "";
-	    if (!function_exists('xml_parser_create'))
-	    {
-	        return array ();
-	    }
-	    $parser = xml_parser_create('');
-	    if (!($fp = @ fopen($url, 'rb')))
-	    {
-	        return array ();
-	    }
-	    while (!feof($fp))
-	    {
-	        $contents .= fread($fp, 8192);
-	    }
-	    fclose($fp);
-	    xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8");
-	    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-	    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
-	    xml_parse_into_struct($parser, trim($contents), $xml_values);
-	    xml_parser_free($parser);
-	    if (!$xml_values)
-	        return; //Hmm...
-	    $xml_array = array ();
-	    $parents = array ();
-	    $opened_tags = array ();
-	    $arr = array ();
-	    $current = & $xml_array;
-	    $repeated_tag_index = array ();
-	    foreach ($xml_values as $data)
-	    {
-	        unset ($attributes, $value);
-	        extract($data);
-	        $result = array ();
-	        $attributes_data = array ();
-	        if (isset ($value))
-	        {
-	            if ($priority == 'tag')
-	                $result = $value;
-	            else
-	                $result['value'] = $value;
-	        }
-	        if (isset ($attributes) and $get_attributes)
-	        {
-	            foreach ($attributes as $attr => $val)
-	            {
-	                if ($priority == 'tag')
-	                    $attributes_data[$attr] = $val;
-	                else
-	                    $result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
-	            }
-	        }
-	        if ($type == "open")
-	        {
-	            $parent[$level -1] = & $current;
-	            if (!is_array($current) or (!in_array($tag, array_keys($current))))
-	            {
-	                $current[$tag] = $result;
-	                if ($attributes_data)
-	                    $current[$tag . '_attr'] = $attributes_data;
-	                $repeated_tag_index[$tag . '_' . $level] = 1;
-	                $current = & $current[$tag];
-	            }
-	            else
-	            {
-	                if (isset ($current[$tag][0]))
-	                {
-	                    $current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
-	                    $repeated_tag_index[$tag . '_' . $level]++;
-	                }
-	                else
-	                {
-	                    $current[$tag] = array (
-	                        $current[$tag],
-	                        $result
-	                    );
-	                    $repeated_tag_index[$tag . '_' . $level] = 2;
-	                    if (isset ($current[$tag . '_attr']))
-	                    {
-	                        $current[$tag]['0_attr'] = $current[$tag . '_attr'];
-	                        unset ($current[$tag . '_attr']);
-	                    }
-	                }
-	                $last_item_index = $repeated_tag_index[$tag . '_' . $level] - 1;
-	                $current = & $current[$tag][$last_item_index];
-	            }
-	        }
-	        elseif ($type == "complete")
-	        {
-	            if (!isset ($current[$tag]))
-	            {
-	                $current[$tag] = $result;
-	                $repeated_tag_index[$tag . '_' . $level] = 1;
-	                if ($priority == 'tag' and $attributes_data)
-	                    $current[$tag . '_attr'] = $attributes_data;
-	            }
-	            else
-	            {
-	                if (isset ($current[$tag][0]) and is_array($current[$tag]))
-	                {
-	                    $current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
-	                    if ($priority == 'tag' and $get_attributes and $attributes_data)
-	                    {
-	                        $current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
-	                    }
-	                    $repeated_tag_index[$tag . '_' . $level]++;
-	                }
-	                else
-	                {
-	                    $current[$tag] = array (
-	                        $current[$tag],
-	                        $result
-	                    );
-	                    $repeated_tag_index[$tag . '_' . $level] = 1;
-	                    if ($priority == 'tag' and $get_attributes)
-	                    {
-	                        if (isset ($current[$tag . '_attr']))
-	                        {
-	                            $current[$tag]['0_attr'] = $current[$tag . '_attr'];
-	                            unset ($current[$tag . '_attr']);
-	                        }
-	                        if ($attributes_data)
-	                        {
-	                            $current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
-	                        }
-	                    }
-	                    $repeated_tag_index[$tag . '_' . $level]++; //0 and 1 index is already taken
-	                }
-	            }
-	        }
-	        elseif ($type == 'close')
-	        {
-	            $current = & $parent[$level -1];
-	        }
-	    }
-	    return ($xml_array);
 	}
 }
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: sebmos
 Donate link: http://sebmos.at/factolex-wordpress-plugin/
 Tags: factolex, tag
 Requires at least: 2.5
-Tested up to: 2.7
+Tested up to: 2.7.1
 Stable tag: trunk
 
 Factolex is a fact lexicon with an API that allows retrieving facts and use them on your blog. This plugin makes showing facts on a blog very simple.
@@ -21,16 +21,65 @@ Facts from Factolex are available in **English** and **German**.
 
 To *install* the plug-in, copy the factolex.php-file into the wp-content/plugins-folder and activate the plug-in in the WordPress Plugin Administration.
 
-To *use* the plugin, write the following code to the template file that should
-show the facts:
+There are two ways to *get data* and two ways to *use the data* with the plugin, they require editing your theme's template files.
 
-`<?php 
-$factolex = new Factolex_Facts($keyword, $number_of_facts (default: 3), $language (en|de));
-echo $factolex->getHtml();
-?>`
+**Step 1: Initialize Plugin**
 
-To use the tag pages tag, use the following line:
+`$factolex = new Factolex_Facts();`
+
+**Step 2: Get Data** 
+
+*2.1 Get Terms by Name*
+
+Get only those terms that equal the search query.
+
+`$factolex->getByTerm($query, $language (en|de));`
+
+*2.2 Get Term by ID*
+
+Get one term based on Factolex' internal term ID.
+
+`$factolex->getById($id);`
+
+*2.3 Search for Terms*
+
+Returns all terms, including those that don't exactly equal the search query.
+
+`$factolex->search($query, $language (en|de));`
+
+**Step 3: Return the Data**
+
+*3.1 Return HTML*
+
+`$factolex->getHTML($maximum_facts);`
+
+*3.2 Return Array*
+
+`$factolex->getArray();`
+
+`
+$data = array(
+	array(
+		'title'	=> 'Term title',
+		'id'	=> 'Internal Term ID',
+		'link'	=> 'Link to term on Factolex homepage',
+		'tags'	=> 'Tags, separated by spaces',
+		'facts'	=> array(
+			array(
+				'title'		=> 'Fact text',
+				'tags'		=> 'Tags, separated by spaces',
+				'source'	=> 'Link to source, if available'
+			)
+		)
+	)
+);`
+
+**Example**
+
+To search for terms on the tag page and return the three best ones.
 
 `<?php
-$keyword = single_tag_title('', false);
+$factolex = new Factolex_Facts();
+$factolex->getByTerm(single_tag_title('', false), 'en');
+echo $factolex->getHtml(3);
 ?>`
